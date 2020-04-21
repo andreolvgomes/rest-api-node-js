b6acedb47f43e93ad7d2b60fb87489e78bb04fb1
André Oliveira <inforoliver@gmail.com>
2020-04-21 19:22:04 -0300

    Login + JWT Token

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -2,12 +2,15 @@ const express = require('express');
 const router = express.Router();
 const mysql = require('../mysql');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const JWT_KEY = "segredo"
 
 router.post('/cadastro', (req, res, next) => {
     bcrypt.hash(req.body.senha, 10, (errBcrypt, hash) => {
         if (errBcrypt) {
             return res.status(500).send({ error: errBcrypt })
         }
+
         mysql.query('select * from usuarios where email = ?', [req.body.email],
             (error, results) => {
                 if (error) { return res.status(500).send({ error: error }) }
@@ -29,6 +32,38 @@ router.post('/cadastro', (req, res, next) => {
                     });
             });
     });
+});
+
+router.post('/login', (req, res, next) => {
+    mysql.query('select * from usuarios where email = ?', [req.body.email],
+        (error, results) => {
+            if (error) { return res.status(500).send({ error: error }) }
+
+            if (results.length == 0) {
+                return res.status(401).send({ mensagem: 'Falha na autenticação', })
+            }
+
+            bcrypt.compare(req.body.senha, results[0].senha, (err, result) => {
+                if (error) {
+                    return res.status(500).send({ error: err })
+                }
+
+                if (result) {
+                    let token = jwt.sign({
+                        id_usuario: results[0].id_usuario,
+                        email: results[0].email
+                    }, JWT_KEY, {
+                        expiresIn: "1h"
+                    });
+
+                    return res.status(200).send({
+                        mensagem: "Autenticado com sucesso",
+                        token: token
+                    })
+                }
+                return res.status(401).send({ mensagem: "Falha na autenticação" })
+            });
+        });
 });
 
 module.exports = router;
